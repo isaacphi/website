29c5633de8c4f3b1ed5b9c50a74373995f0d5c2a
Phil Isaac <isaac.phil@gmail.com>
2014-08-15 15:42:58 -0700

    change from marku to kramdown, begin experiment folder

---
+++ js/display-video.js
@@ -0,0 +1,31 @@
+console.log("loaded");
+
+navigator.getUserMedia = ( navigator.getUserMedia ||
+			   navigator.webkitGetUserMedia ||
+			   navigator.mozGetUserMedia ||
+			   navigator.msGetUserMedia);
+
+if (navigator.getUserMedia) {
+    console.log("getUserMedia supported");
+    navigator.getUserMedia (
+
+	// constraints
+	{
+            video: true
+	},
+
+	// successCallback
+	function(localMediaStream) {
+            var video = document.querySelector('video');
+            video.src = window.URL.createObjectURL(localMediaStream);
+	},
+
+	// errorCallback
+	function(err) {
+            console.log("The following error occured: " + err);
+	}
+    );
+} else {
+    console.log("getUserMedia not supported");
+}
+
